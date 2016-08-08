48d079ab02e3eca32fc1b88fdcbb7d0b7372ae77
josephferrairo <josephferrairo@gmail.com>
2016-08-07 22:12:53 -0400

    Doctors displayed in form now have same category id as patient

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,18 @@
 class AppointmentsController < ApplicationController
+
+  def new
+    @patient = Patient.find(params[:patient_id])
+    @appointment = Appointment.new
+  end
+
+  def create
+    @patient = Patient.find(params[:patient_id])
+    @appointment = @patient.appointments.create(appointment_params)
+    redirect_to patient_path(@patient)
+  end
+
+  private
+  def appointment_params
+    params.require(:appointment).permit(:start_date, :start_time, :doctor_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :doctors
-  resources :patients
+  resources :patients do
+    resources :appointments
+  end
   resources :categories
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
