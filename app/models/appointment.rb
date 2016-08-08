1bb159a57894c272056294c9bd80bbbfdf27677c
josephferrairo <josephferrairo@gmail.com>
2016-08-08 00:59:02 -0400

    added after_create to appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,10 +3,15 @@ class Appointment < ActiveRecord::Base
   belongs_to :doctor
   validates :start_date, :start_time, :doctor_id, presence: true
   validate :three_days_away
+  after_create :send_appointment_mailer
 
   def three_days_away
     if start_date <= 2.days.from_now
       errors.add(:start_date, "must be 3 days away")
     end
+  end
+
+  def send_appointment_mailer
+    AppointmentNotifier.send_appointment_email(self).deliver
   end
 end
