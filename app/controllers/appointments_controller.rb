e557dc03dc75ce8fc8c26dc70db26345fd524691
josephferrairo <josephferrairo@gmail.com>
2016-08-07 22:46:33 -0400

    added validation for appointments so that they are 3 days away

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -8,7 +8,16 @@ class AppointmentsController < ApplicationController
   def create
     @patient = Patient.find(params[:patient_id])
     @appointment = @patient.appointments.create(appointment_params)
-    redirect_to patient_path(@patient)
+
+    respond_to do |format|
+      if @appointment.save
+        format.html { redirect_to @patient, notice: 'Patient was successfully created.' }
+        format.json { render :show, status: :created, location: @appointment }
+      else
+        format.html { render :new }
+        format.json { render json: @appointment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,12 @@
 class Appointment < ActiveRecord::Base
   belongs_to :patient
   belongs_to :doctor
+  validates :start_date, :start_time, :doctor_id, presence: true
+  validate :three_days_away
+
+  def three_days_away
+    if start_date <= 2.days.from_now
+      errors.add(:start_date, "must be 3 days away")
+    end
+  end
 end
