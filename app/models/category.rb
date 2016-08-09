b8523dc1cdf2bc12c7a1f24eb690b6d4d3af2af1
josephferrairo <josephferrairo@gmail.com>
2016-08-08 23:08:25 -0400

    refactored category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ActiveRecord::Base
-  validates :specialty, presence: true
-  validates :ailment, presence: true
+  validates :specialty, :ailment, presence: true
   has_many :doctors
   has_many :patients
 end
